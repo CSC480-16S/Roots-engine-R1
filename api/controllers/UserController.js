d8ad12b516139c4349e739bee95cbb5ecf243a64
nedingto <nedingto@oswego.edu>
2016-02-12 15:24:33 -0500

    Basic View to Controller  to Model connection

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,7 +9,7 @@ module.exports = {
   create: function(req, res) {
     var params = req.params.all()
     User.create({user_name: params.email, password: params.password}).exec(function createCB(err, created) {
-      console.log('Created user with name ' + created.name);
+      console.log('Created user with name ' + params.email);
     });
   }
 };
