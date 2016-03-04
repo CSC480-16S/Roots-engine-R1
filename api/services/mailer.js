bdd881a3371973146e86a74f023e5127b3945161
Jared Brower <jbrower@oswego.edu>
2016-03-03 22:14:35 -0500

    password exclusion

--- api/services/mailer.js
+++ api/services/mailer.js
@@ -1,9 +1,12 @@
 
 module.exports = {
     send: function(address, sub, txt, htm, next) {
-        var nodemailer = require('nodemailer'),
+        var nodemailer = require('nodemailer');
             // manually put in password below to get functional
-            transporter = nodemailer.createTransport('smtps://rootsspammer%40gmail.com:roots480@smtp.gmail.com'),
+	var emailPass = passwords.emailPass();
+	var pw = require("../../config/passwords.json");
+	var ePass = pw.emailPass;
+	var transporter = nodemailer.createTransport('smtps://rootsspammer%40gmail.com:' + ePass + '@smtp.gmail.com'),
             mailOptions = {
                 from: 'Roots Team <rootsspammer@gmail.com>',
                 to: address,
